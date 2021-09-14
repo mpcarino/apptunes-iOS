f5486bbcc2dd09309a832f86ba15ec61c6bef443
marwincarino <81006256+mpscarino101@users.noreply.github.com>
2021-09-14 10:50:34 +0800

    Add BorderProtocol

---
+++ Apptunes/Apptunes/Helpers/BorderProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  BorderProtocol.swift
+//  Apptunes
+//
+//  Created by Marwin Carino on 9/14/21.
+//
+
+import Foundation
+import UIKit
+
+protocol BorderProtocol {
+    var borderWidth: CGFloat { get set }
+    var borderColor: UIColor { get set }
+}
