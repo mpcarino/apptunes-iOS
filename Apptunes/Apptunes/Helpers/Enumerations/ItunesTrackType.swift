4778a92d145a4d1df8252bf1f2aacd636ae7eb5a
marwincarino <81006256+mpscarino101@users.noreply.github.com>
2021-09-14 11:28:55 +0800

    Add ItunesTrackType enum

---
+++ Apptunes/Apptunes/Helpers/Enumerations/ItunesTrackType.swift
@@ -0,0 +1,13 @@
+//
+//  ItunesTrackType.swift
+//  Apptunes
+//
+//  Created by Marwin Carino on 9/14/21.
+//
+
+import Foundation
+
+enum ItunesTrackType: String {
+    case song
+    case movie = "feature-movie"
+}
